742e38003828ece08abd370e77991748b57f1cb4
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-15 20:29:54 -0700

    automatic commit message

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '/../public');
 const app = express();
+const socketIO = require('socket.io');
+const PORT = process.env.PORT || 3000;
 
+const server = http.createServer(app);
 app.use(express.static(publicPath));
-app.listen(process.env.PORT || 3000, () => {
+server.listen(PORT, () => {
     console.log("Server is up on port : 3000");
 })
