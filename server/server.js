cd467b3cf1599665909d67782baf1555bab0d7c8
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-15 20:38:45 -0700

    automatic commit message

--- server/server.js
+++ server/server.js
@@ -9,6 +9,12 @@ const server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+//Start and Listen
 server.listen(PORT, () => {
     console.log(`Server is up on port : ${PORT}`);
-})
+});
+
+io.on('connection', (socket) => {
+    console.log("Connection Established");
+});
