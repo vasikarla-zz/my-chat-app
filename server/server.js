27ee2c58bd97ef096c5d3d84a9a4f29607eaf269
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-16 21:49:33 -0700

    automatic commit message

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const app = express();
 const socketIO = require('socket.io');
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
+const {generateMessage} = require('./utils/message');
 var os= require('os');
 var io = socketIO(server);
 
@@ -16,24 +17,13 @@ io.on("connection", (socket) => {
     console.log("Server Connection : On")
 
     //Welcome to Chat App
-    socket.emit("newMessage", {
-        from: "Admin",
-        text: "Welcome to the Chat App"
-    });
-
-    socket.broadcast.emit("newMessage", {
-        from: "Admin",
-        text: "New User Joined" + os.userInfo().toString()
-    });
+    socket.emit("newMessage", generateMessage("Admin", "Welcome to Chat App"));
+    socket.broadcast.emit("newMessage", generateMessage("Admin", "New User Joined"));
 
     //Custom Events
     socket.on("createMessage", (message) => {
         console.log("Create Message Event", message);
-        socket.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        }) 
+        socket.emit('newMessage', generateMessage(message.from, message.text)) 
     });
 
     socket.on("disconnect", () => {
