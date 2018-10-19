13c1ec105cdf7667e19ff55e71604c6d4ec68d47
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-19 16:02:56 -0700

    automatic commit message

--- server/server.js
+++ server/server.js
@@ -17,17 +17,12 @@ io.on('connection', (socket) => {
 
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined : ' + require("os").userInfo().username));
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from the server.');
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
   });
 
   socket.on('disconnect', () => {
