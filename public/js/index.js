7f7cbacfabb0a724dc5f36e04165e1498c65d670
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-19 15:59:46 -0700

    automatic commit message

--- public/js/index.js
+++ public/js/index.js
@@ -10,29 +10,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
-  var list = jQuery("<li></li>");
-  list.text(`${message}`);
-  jQuery("messages").append(list);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'Hi'
-}, function (data) {
-  console.log('Got it', data);
-});
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
 
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
 
-$("#message-form").on('submit', function (e) {
-    console.log(e);
-    e.preventDefault();
-
-
-    socket.emit('createMessage', {
-        from: "User",
-        text: jQuery("#message").val()
-    }, function () {
-        console.log("Ack");
-    });
-
+  });
 });
